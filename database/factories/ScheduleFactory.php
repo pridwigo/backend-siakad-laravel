fe0a5c7e1d0c126468cfdca99aaebd2cc6387b53
pridwigo <primantorodev@outlook.com>
2024-12-17 22:43:21 +0700

    Laravel API, Auth User, API Get Schedules by User Login, Migration Subject & Schedule

---
+++ database/factories/ScheduleFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use DateTime;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Schedule>
+ */
+class ScheduleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'student_id'=>1,
+            'subject_id'=>5,
+            'schedule_date'=>fake()->DateTime(),
+            'schedule_type'=>'online',
+        ];
+    }
+}
